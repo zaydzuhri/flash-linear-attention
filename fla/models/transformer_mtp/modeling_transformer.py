7b807f9afa6db912984411961d53ab3244147dbb
zaydzuhri <zaydzuhri@gmail.com>
2025-11-06 06:42:58 +0000

    [MTP] add argument "output_mtp_logits"

--- fla/models/transformer_mtp/modeling_transformer.py
+++ fla/models/transformer_mtp/modeling_transformer.py
@@ -529,7 +529,7 @@ class MTPTransformerForCausalLM(MTPTransformerPreTrainedModel, GenerationMixin):
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
+        return_all_heads = self.training or ('output_mtp_logits' in kwargs and kwargs['output_mtp_logits'])
         outputs = self.model(
             input_ids=input_ids,
             attention_mask=attention_mask,
@@ -539,7 +539,7 @@ class MTPTransformerForCausalLM(MTPTransformerPreTrainedModel, GenerationMixin):
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
-            return_all_heads=self.training,
+            return_all_heads=return_all_heads,
             **kwargs
         )
 
